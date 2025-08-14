23dce708ac5480e6ce83fd48b1904e815caefa98
Pa04rth <parth.22440@knit.ac.in>
2025-08-14 15:39:19 +0530

    Added project

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -184,8 +184,8 @@ export const DATA = {
       video: "/trading.mp4",
     },
     {
-      title: "Magic UI",
-      href: "https://magicui.design",
+      title: "AIVSS Dashboard",
+      href: "https://aivss.parthsohaney.online/",
       dates: "June 2023 - Present",
       active: true,
       description:
@@ -199,83 +199,22 @@ export const DATA = {
         "Stripe",
         "Shadcn UI",
         "Magic UI",
+        "OWASP Top 10",
       ],
       links: [
         {
           type: "Website",
-          href: "https://magicui.design",
+          href: "https://aivss.parthsohaney.online/",
           icon: <Icons.globe className="size-3" />,
         },
         {
           type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
+          href: "https://github.com/Pa04rth/agentrisk-insight",
           icon: <Icons.github className="size-3" />,
         },
       ],
       image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
-    {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
-      active: true,
-      description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://llm.report",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
-    },
-    {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
-      active: true,
-      description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://automatic.chat",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+      video: "/aivss.mp4",
     },
   ],
   hackathons: [
